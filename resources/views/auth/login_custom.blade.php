c925571d2b58e88a5c83aae931ccdc02e8f4fa17
Elite X2 <elite@example.com>
2025-10-15 11:29:46 +0700

    Simpan perubahan pada login_custom.blade.php

--- resources/views/auth/login_custom.blade.php
+++ resources/views/auth/login_custom.blade.php
@@ -126,6 +126,11 @@
             <input type="password" name="password" class="form-control" required autocomplete="current-password">
         </div>
 
+        <div class="mb-3 form-check">
+            <input type="checkbox" name="remember" class="form-check-input" id="remember">
+            <label class="form-check-label" for="remember">Ingat Saya</label>
+        </div>
+
         <button type="submit" class="btn btn-primary w-100 mt-3">Masuk</button>
     </form>
 
